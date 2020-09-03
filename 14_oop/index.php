08fdccfbd7485f008d9ede05b3700a9a927eced9
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-09-03 10:38:33 +0630

    update static

--- 14_oop/index.php
+++ 14_oop/index.php
@@ -87,12 +87,132 @@
 // }
 
 // $person = new Person;
+
+// $person->sayHello();
+// $person = new class {
+//     public function sayHello() {
+//         echo 'hello';
+//     }
+// };
 // $person->sayHello();
 
-$person = new class {
-    public function sayHello() {
-        echo 'hello';
-    }
-};
+// class Person {
+//     public $name;
+//     protected $age;
+//     private $email;
 
-$person->sayHello();
+//     public function __construct($name,$age,$email) {
+//         echo 'constructor'.'<br>';
+//         $this->name = $name;
+//         $this->age = $age;
+//         $this->email = $email;
+//     }
+//     public function sayHello() {
+//         return 'Hello';
+//     }
+//     public function getName() {
+//         return $this->name;
+//     }
+//     public function setName($name) {
+//         $this->name = $name;
+//     }
+//     public function getEmail() {
+//         return $this->email;
+//     }
+//     public function __destruct() {
+//         echo 'destructor'.'<br>';
+//     }
+// }
+
+// class Student extends Person {
+//     public function getAge() {
+//         return $this->age;
+//     }
+// }
+
+// $person = new Person('Mg Mg',22,'mgmg@gmail.com');
+// echo $person->getName();
+// echo '<br>';
+// echo $person->setName('Kyaw Kyaw');
+// echo $person->getName();
+// echo '<br>';
+
+// $person = new Person('Khin Khin',12,'khin@gmail.com');
+// echo $person->getName();
+// echo '<br>';
+
+// $student = new Student('Mg Mg',22,'mgmg@gmail.com');
+// echo $student->getAge();
+
+class Person {
+
+    public $name;
+    public $email;
+
+    public static $age = 22;
+
+    public function __construct($name,$email) {
+        // echo 'I am constructor <br>';
+        $this->name = $name;
+        $this->email = $email;
+        
+    }
+
+    public static function getAge() {
+        return self::$age;
+    }
+
+    public function getName() {
+        return self::getAge();
+        // return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function __destruct() {
+        // echo 'I am destructor <br>';
+    }
+
+}
+
+
+// echo Person::$age;
+// echo Person::$name;
+// echo Person::getAge();
+
+$person = new Person('Mg Mg','mgmg@gmail.com');
+echo $person->getName() . '<br>';
+
+// echo $person->getAge();
+
+// class Student extends Person {
+//     public $roll_no;
+//     public function __construct($name,$email,$roll_no) {
+//         parent::__construct($name,$email);
+//         $this->roll_no = $roll_no;
+//     }
+//     public function hello() {
+//         return "Hello Mg Mg";
+//     }
+// }
+
+// $person = new Person('Mg Mg');
+// // echo $person->name;
+// echo $person->getName();
+// echo '<br>';
+// $person->setName('Aung Aung');
+// echo $person->getName();
+// echo '<br>';
+// unset($person);
+
+// echo 'Something <br>';
+
+// $student = new Student('Mg Mg','mgmg@gmail.com','7CE-11');
+// echo $student->name;
+// echo '<br>';
+// echo $student->email;
+// echo '<br>';
+// echo $student->roll_no;
+// echo '<br>';
